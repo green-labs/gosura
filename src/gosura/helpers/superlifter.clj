c9e6a107f67e25754333fd2fbe0992f992ca1000
Hoseong Son <hs.son@greenlabs.co.kr>
2022-07-13 19:45:04 +0900

    superfetcher-v2 생성

--- src/gosura/helpers/superlifter.clj
+++ src/gosura/helpers/superlifter.clj
@@ -40,7 +40,6 @@
                                    etc-keys) ; 인증 관련되어서 추가 필터를 적용하기 위함
         filter-options (merge (->> arguments-list first :filter-options) ; base filter options
                               {filter-key ids}
-                              {:batch-args arguments-list} ; 추가 필터값이 포함된 전체 argument list
                               etc-filter-options)
         page-options (-> (->> arguments-list first :page-options) ; base page options
                          (dissoc :limit))  ; (연오) foolproof: 페치할 때 LIMIT 하면 안 된다. 페치 -> ID별 그룹 -> 그룹별 LIMIT
@@ -62,3 +61,40 @@
     `(api/def-superfetcher ~name [~id ~arguments]
        (fn [many# env#]
          (superfetch many# env# ~params)))))
+
+(defn superfetch-v2
+  "
+  ## 인자
+  * id-column   superfetch 결과의 매핑 단위가 되는 (parent 하나에 대응되는) 컬럼 이름
+  * filter-key  table-fetcher에서 in 조건을 걸어줄 컬럼 이름"
+  [many
+   env
+   {:keys [db-key table-fetcher id-column filter-key]}]
+  (let [db (get env db-key)
+        arguments-list (map :arguments many)
+        ids (->> arguments-list
+                 (map :id)
+                 (map str))
+        filter-options (merge (->> arguments-list first :filter-options) ; base filter options
+                              {:batch-args arguments-list} ; 추가 필터값이 포함된 전체 argument list
+                              {filter-key ids})
+        page-options (-> (->> arguments-list first :page-options) ; base page options
+                         (dissoc :limit))  ; (연오) foolproof: 페치할 때 LIMIT 하면 안 된다. 페치 -> ID별 그룹 -> 그룹별 LIMIT
+        id->rows (->> (table-fetcher db filter-options page-options)
+                      (map #(update % id-column str))  ; ids가 str로 입력되므로 맞춤
+                      (group-by id-column))]
+    (map id->rows ids))) ; rows를 ids 순서대로 배치
+
+(defmacro superfetcher-v2
+  "superfetcher 기본 form을 쉽게 제공한다
+   defrecord를 생성하므로 name은 PascalCase로 작성하는 걸 원칙으로 한다
+   호출할 때에는 ->name 으로 사용한다
+   생성예시) (superfetcher FetchByExampleId {...})
+   사용예시) ->FetchByExampleId
+   "
+  [name params]
+  (let [id (symbol "id")
+        arguments (symbol "arguments")]
+    `(api/def-superfetcher ~name [~id ~arguments]
+       (fn [many# env#]
+         (superfetch-v2 many# env# ~params)))))
