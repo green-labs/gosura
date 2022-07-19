1ea1e2d172e855d183a3a69b22873869b0f008ba
Hoseong Son <hs.son@greenlabs.co.kr>
2022-07-19 20:50:25 +0900

    filter-options 테스트 케이스 추가

--- test/gosura/helpers/superlifter_test.clj
+++ test/gosura/helpers/superlifter_test.clj
@@ -2,12 +2,13 @@
   (:require [clojure.test :refer [deftest are testing run-tests]]
             [gosura.helpers.superlifter :refer [superfetch-v2]]))
 
-(def superlifter-requests [{:id 1 :arguments {:country-code "JP" :id "1204" :page-options nil}}
+(def superlifter-requests [{:id 1 :arguments {:country-code "JP" :id "1204" :page-options nil :filter-options [{:test-filter "filter"}]}}
                            {:id 2 :arguments {:country-code "JP" :id "1123" :page-options nil}}
                            {:id 3 :arguments {:country-code "JP" :id "370" :page-options nil}}])
 
 (deftest superlifter-v2-test
-  (testing "superlifter 요청을 받아서 table-fetcher로 올바른 batch-args를 전달해줍니다."
+  (testing "superlifter 요청을 받아서 db에서 값을 fetch해 올바른 순서로 반환합니다.
+            table-fetcher로 올바른 batch-args를 전달해줍니다."
     (let [filter-option-args (atom {})
           args {:db-key :db
                 :table-fetcher (fn [_db filter-options _]
@@ -18,10 +19,11 @@
                 :id-in-parent :id}
           result (superfetch-v2 superlifter-requests {} args)]
       (are [expected target] (= expected target)
-        @filter-option-args {:batch-args [{:country-code "JP" :id "1204"}
-                                          {:country-code "JP" :id "1123"}
-                                          {:country-code "JP" :id "370"}]}
-        result '([{ :id "1204" :title "title1"}] [{:id "1123" :title "title3"}] [{:id "370" :title "title2"}])))))
+        @filter-option-args [{:test-filter "filter"}
+                             {:batch-args [{:country-code "JP" :id "1204" :filter-options [{:test-filter "filter"}]}
+                                           {:country-code "JP" :id "1123"}
+                                           {:country-code "JP" :id "370"}]}]
+        result '([{:id "1204" :title "title1"}] [{:id "1123" :title "title3"}] [{:id "370" :title "title2"}])))))
 
 (comment
   (run-tests))
