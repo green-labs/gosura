fc78ede64a0660eb86e1ccd6fffe1815ca17807a
jungwoo <50188177+jungwookim@users.noreply.github.com>
2022-09-01 11:16:37 +0900

    fix schema

--- src/gosura/schema.clj
+++ src/gosura/schema.clj
@@ -2,7 +2,7 @@
 
 (def resolver-settings-schema
   [:map {:closed true}
-   [:auth {:optional true} [:or symbol? [:cat symbol? [:* any?]]]]
+   [:auth {:optional true} [:or [:or symbol? var?] [:cat [:or symbol? var?] [:* any?]]]]
    [:kebab-case? {:optional true} boolean?]
    [:return-camel-case? {:optional true} boolean?]])
 
@@ -12,10 +12,10 @@
    [:settings {:optional true} resolver-settings-schema]
    [:node-type {:optional true} keyword?]
    [:db-key {:optional true} keyword?]
-   [:post-process-row {:optional true} symbol?]
-   [:pre-process-arguments {:optional true} symbol?]
-   [:table-fetcher symbol?]
-   [:mutation-fn symbol?]
+   [:post-process-row {:optional true} [:or symbol? var?]]
+   [:pre-process-arguments {:optional true} [:or symbol? var?]]
+   [:table-fetcher [:or symbol? var?]]
+   [:mutation-fn [:or symbol? var?]]
    [:mutation-tag keyword?]])
 
 (def base-root-query-multi-config-schema
@@ -23,28 +23,28 @@
    [:settings {:optional true} resolver-settings-schema]
    [:node-type {:optional true} keyword?]
    [:db-key {:optional true} keyword?]
-   [:post-process-row {:optional true} symbol?]
-   [:pre-process-arguments {:optional true} symbol?]
-   [:table-fetcher symbol?]])
+   [:post-process-row {:optional true} [:or symbol? var?]]
+   [:pre-process-arguments {:optional true} [:or symbol? var?]]
+   [:table-fetcher [:or symbol? var?]]])
 
 (def base-root-query-one-config-schema
   [:map
    [:settings {:optional true} resolver-settings-schema]
    [:node-type {:optional true} keyword?]
    [:db-key {:optional true} keyword?]
-   [:post-process-row {:optional true} symbol?]
-   [:pre-process-arguments {:optional true} symbol?]
-   [:fetch-one symbol?]])
+   [:post-process-row {:optional true} [:or symbol? var?]]
+   [:pre-process-arguments {:optional true} [:or symbol? var?]]
+   [:fetch-one [:or symbol? var?]]])
 
 (def resolvers-map-schema
   "resolvers 설정에 필요한 스키마 정의"
   [:map {:closed true}
-   [:target-ns symbol?]
+   [:target-ns [:or symbol? var?]]
    [:node-type keyword?]
    [:db-key keyword?]
-   [:post-process-row {:optional true} symbol?]
+   [:post-process-row {:optional true} [:or symbol? var?]]
    [:filters {:optional true} map?]
-   [:pre-process-arguments {:optional true} symbol?]
+   [:pre-process-arguments {:optional true} [:or symbol? var?]]
    [:resolvers [:map
                 [:resolve-connection {:optional true} base-root-query-multi-config-schema]
                 [:resolve-one {:optional true} base-root-query-one-config-schema]
@@ -52,8 +52,8 @@
                                                   [:settings {:optional true} resolver-settings-schema]
                                                   [:node-type {:optional true} keyword?]
                                                   [:db-key {:optional true} keyword?]
-                                                  [:post-process-row {:optional true} symbol?]
-                                                  [:superfetcher symbol?]
+                                                  [:post-process-row {:optional true} [:or symbol? var?]]
+                                                  [:superfetcher [:or symbol? var?]]
                                                   [:fk-in-parent keyword?]]]
                 [:resolve-create-one {:optional true} base-mutation-config-schema]
                 [:resolve-update-one {:optional true} base-mutation-config-schema]
@@ -61,7 +61,7 @@
                 [:resolve-delete-one {:optional true} [:map
                                                        [:settings {:optional true} resolver-settings-schema]
                                                        [:db-key {:optional true} keyword?]
-                                                       [:mutation-fn symbol?]]]]]])
+                                                       [:mutation-fn [:or symbol? var?]]]]]]])
 
 (def node-schema
   [:map
