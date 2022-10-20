5df09c6f3fefa8b010388709f8a4abaccac50dbd
Hyunwoo Nam <namenu@gmail.com>
2022-10-20 15:30:03 +0900

    tagged literal 지원을 위해 사용되는 aero 제거

--- src/gosura/edn.clj
+++ src/gosura/edn.clj
@@ -1,10 +1,14 @@
 (ns gosura.edn
-  (:require [aero.core :as aero]))
-
-(defmethod aero/reader 'var
-  [_opts _tag value]
-  (requiring-resolve value))
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io])
+  (:import (java.io PushbackReader)))
 
 (defn read-config
+  "edn 형식의 설정파일을 불러들임.
+  tagged literal `#var`를 지원하기 위해 사용한다.
+  "
   [path]
-  (aero/read-config path))
+  (with-open [r (io/reader path)]
+    (edn/read {:readers (merge default-data-readers
+                               {'var requiring-resolve})}
+              (PushbackReader. r))))
