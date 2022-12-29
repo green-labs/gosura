dbfa02e578382554610aad504dfae38728dab137
wickedev <orange881217@gmail.com>
2022-12-29 14:18:30 +0900

    오류 수정

--- src/gosura/helpers/error.clj
+++ src/gosura/helpers/error.clj
@@ -1,6 +1,5 @@
 (ns gosura.helpers.error
-  (:require [com.walmartlabs.lacinia.resolve :refer [resolve-as]]
-            [gosura.helpers.error :as error]))
+  (:require [com.walmartlabs.lacinia.resolve :refer [resolve-as]]))
 
 (defn error
   "resolve-as를 사용하여 error를 반환하고 기본적으로 값으로는 nil을 반환하도록 한다"
