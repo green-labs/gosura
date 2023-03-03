49910cb91c774ee1d8af51eed984d9547da71446
wickedev <orange881217@gmail.com>
2023-03-03 13:26:59 +0900

    fix order-by case

--- src/gosura/helpers/relay.clj
+++ src/gosura/helpers/relay.clj
@@ -2,6 +2,7 @@
   (:require [clojure.string]
             [gosura.case-format :as cf]
             [gosura.schema :as gosura-schema]
+            [camel-snake-kebab.core :as csk]
             [gosura.util :refer [stringify-ids]]
             [malli.core :as m]
             [ring.util.codec :refer [base64-decode base64-encode]]
@@ -226,7 +227,7 @@
         cursor-ordered-values (when-not offset-based-pagination (:ordered-values cursor))
         cursor-id (when-not offset-based-pagination (:id cursor))
         offset (when offset-based-pagination (:offset cursor))
-        order-by  (cond-> order-by kebab-case? cf/camelCaseKeyword->kebab-case-keyword)
+        order-by  (cond-> order-by kebab-case? csk/->kebab-case-keyword)
         order-direction (cf/UPPER_SNAKE_CASE_KEYWORD->kebab-case-keyword order-direction) ; ASC/DESC -> asc/desc
         order-direction (get #{:asc :desc} order-direction :asc)
         order-direction (case page-direction
